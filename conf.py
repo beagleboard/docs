144ef88dffd02d4e2ae275c3992e94c27b76eb37
Jason Kridner <jkridner@beagleboard.org>
2022-01-31 11:29:04 -0500

    Add asciidoc

--- conf.py
+++ conf.py
@@ -69,7 +69,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.md', '.adoc', '.asciidoc']
 
 # The master toctree document.
 master_doc = 'index'
